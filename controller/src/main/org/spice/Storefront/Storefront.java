91bd434294cd4c2da02ccc0c80f6aa5108cfa1fc
Kenneth <kenneth.rosario2@knights.ucf.edu>
2020-04-15 00:59:21 -0400

    update storefront

---
+++ controller/src/main/org/spice/Storefront/Storefront.java
@@ -0,0 +1,25 @@
+package org.spice.Storefront;
+
+import java.util.Map;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.json.Json;
+import javax.json.JsonWriter;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonArrayBuilder;
+import java.util.function.BiConsumer;
+
+import org.spice.rest.RESTServlet;
+import org.spice.rest.RESTException;
+
+public class Storefront extends RESTServlet {
+
+    public void createDiscountCode(HttpServletRequest request, HttpServletResponse response) {
+        String disCode;
+        
+
+    }
+
+
+}
